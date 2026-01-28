1424f1d5c849b23ddc37dc066cf355f800498de8
Timothy Laubscher <timlaubscher5063@gmail.com>
2026-01-27 20:15:47 -0500

    Corrected codea and codeb to be single lines

---
+++ index.js
@@ -0,0 +1,13 @@
+let code1;
+code1 = 7 + 3;
+
+let code2 = 90 / 2 - 5;
+
+let code3 = (30 % 8) + 33;
+
+let message = "The vault has been secured. The combination is:";
+
+let codea = code1 + "-" + code2 + "-" + code3;
+let codeb = `${code1}-${code2}-${code3}`;
+
+console.log(message, codea, codeb);
